2257f30c8588d7d4724df8c7632213644f9756ce
Mostafa Oraei <taha.oraei@gmail.com>
2023-09-26 13:35:45 +0330

    add basic config package

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,13 @@
 package config
+
+import (
+	"injam/delivery/httpserver"
+	"injam/repository/postgres"
+	"injam/service/userservice"
+)
+
+type Config struct {
+	Postgres    postgres.Config
+	UserService userservice.Config
+	HTTPSever   httpserver.Config
+}
