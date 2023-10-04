e82a5ae6873c2b4754d42c31ddf9694c4a48cfec
Mostafa Oraei <taha.oraei@gmail.com>
2023-10-04 16:50:01 +0330

    first version of bulking locations

--- delivery/httpserver/websocketserver/getlocation.go
+++ delivery/httpserver/websocketserver/getlocation.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"injam/service/authservice"
 	"log/slog"
+	"time"
 )
 
 func (h Handler) getLocation(c echo.Context) error {
@@ -16,12 +17,35 @@ func (h Handler) getLocation(c echo.Context) error {
 		return err
 	}
 	u := c.Get("my_claims").(*authservice.Claims)
+	var locations []string
+	stop := make(chan struct{})
+	t := time.NewTicker(time.Second * 10)
+	go func() {
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				fmt.Println("locations: ", locations)
+				locations = nil
+			case <-stop:
+				fmt.Println("Here")
+				t.Stop()
+				break
+			}
+		}
+	}()
+
 	go func() {
 		defer conn.Close()
 
 		for {
 			msg, _, err := wsutil.ReadClientData(conn)
+			locations = append(locations, string(msg))
+			fmt.Println("shity: ", time.Now(), string(msg))
 			if err != nil {
+				fmt.Println("websocket close")
+				stop <- struct{}{}
+				break
 			}
 			if err = h.redisAdapter.Client().Publish(context.Background(), fmt.Sprintf("taha:%d", u.UserID), string(msg[:])).Err(); err != nil {
 				slog.Error("cant save data in memory")
